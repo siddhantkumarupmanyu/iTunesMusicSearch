1b9b131ef93af935f36c0d196c1ef0fb331cc01c
Siddhant Kumar Upmanyu <siddhantkumarupmanyu@gmail.com>
2021-11-29 01:37:57 +0530

    add test in GalleryFragmentTest.kt

--- app/src/androidTest/java/sku/challenge/itunesmusicsearch/ui/GalleryFragmentTest.kt
+++ app/src/androidTest/java/sku/challenge/itunesmusicsearch/ui/GalleryFragmentTest.kt
@@ -1,8 +1,18 @@
 package sku.challenge.itunesmusicsearch.ui
 
+import android.view.InputDevice
+import android.view.MotionEvent
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.CoordinatesProvider
+import androidx.test.espresso.action.GeneralClickAction
+import androidx.test.espresso.action.Press
+import androidx.test.espresso.action.Tap
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -37,10 +47,38 @@ class GalleryFragmentTest {
     }
 
     @Test
-    fun testSpike() {
-        onView(withId(R.id.search_view)).perform(click())
+    fun clearSearchViewText_WhenClearDrawableIsClicked() {
+        onView(withId(R.id.search_view)).perform(click(), typeText("song name"))
+        onView(withId(R.id.search_view)).check(matches(withText("song name")))
 
-        Thread.sleep(2000)
+        onView(withId(R.id.search_view)).perform(clickPercent(0.9f, 0.5f))
+        onView(withId(R.id.search_view)).check(matches(withText("")))
     }
+
+    // https://stackoverflow.com/a/58841245
+    @Suppress("SameParameterValue")
+    private fun clickPercent(percentageX: Float, percentageY: Float): ViewAction {
+        return GeneralClickAction(
+            Tap.SINGLE,
+            CoordinatesProvider { view ->
+                val screenPos = IntArray(2)
+                view.getLocationOnScreen(screenPos)
+                val w = view.width
+                val h = view.height
+
+                val x = w * percentageX
+                val y = h * percentageY
+
+                val screenX = screenPos[0] + x
+                val screenY = screenPos[1] + y
+
+                floatArrayOf(screenX, screenY)
+            },
+            Press.FINGER,
+            InputDevice.SOURCE_MOUSE,
+            MotionEvent.BUTTON_PRIMARY
+        )
+    }
+
 
 }
