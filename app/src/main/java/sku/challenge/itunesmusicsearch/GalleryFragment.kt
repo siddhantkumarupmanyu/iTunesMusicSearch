1d5744d6ab3fae85327417db75997f1e588a661c
Siddhant Kumar Upmanyu <siddhantkumarupmanyu@gmail.com>
2021-11-28 22:20:56 +0530

    complete Search EditText

--- app/src/main/java/sku/challenge/itunesmusicsearch/GalleryFragment.kt
+++ app/src/main/java/sku/challenge/itunesmusicsearch/GalleryFragment.kt
@@ -1,9 +1,14 @@
 package sku.challenge.itunesmusicsearch
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import sku.challenge.itunesmusicsearch.databinding.FragmentGalleryBinding
@@ -33,6 +38,53 @@ class GalleryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.searchView.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding.searchView.setCompoundDrawablesWithIntrinsicBounds(
+                    0,
+                    0,
+                    R.drawable.close,
+                    0
+                )
+            } else {
+                if (binding.searchView.length() == 0) {
+                    binding.searchView.setCompoundDrawablesWithIntrinsicBounds(
+                        0,
+                        0,
+                        0,
+                        0
+                    )
+                }
+            }
+        }
+
+        binding.searchView.setOnTouchListener { _, event ->
+            if ((event.action == MotionEvent.ACTION_UP)
+                &&
+                (event.rawX >= (binding.searchView.right - binding.searchView.totalPaddingRight))
+            ) {
+                binding.searchView.setText("")
+                true
+            } else {
+                false
+            }
+        }
+
+        binding.searchView.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                binding.searchView.clearFocus()
+                val imm: InputMethodManager? =
+                    requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+
+                imm?.hideSoftInputFromWindow(view.windowToken, 0)
+                true
+            } else {
+                false
+            }
+        }
     }
 
     override fun onDestroyView() {
