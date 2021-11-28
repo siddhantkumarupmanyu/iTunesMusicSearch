009a2ebc715cca65568ecf5f49ae2341e6ee648c
Siddhant Kumar Upmanyu <siddhantkumarupmanyu@gmail.com>
2021-11-28 20:16:45 +0530

    add binding in GalleryFragment.kt
    idk how to make the shadow round in AppBarLayout. leaving it for now

--- app/src/main/java/sku/challenge/itunesmusicsearch/GalleryFragment.kt
+++ app/src/main/java/sku/challenge/itunesmusicsearch/GalleryFragment.kt
@@ -4,23 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import sku.challenge.itunesmusicsearch.databinding.FragmentGalleryBinding
 
 
 class GalleryFragment : Fragment() {
 
 
+    private var _binding: FragmentGalleryBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val binding: FragmentGalleryBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_gallery,
+            container,
+            false
+        )
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
