3eae1e7048decd3e2381ef917a568834186ca7b3
Siddhant Kumar Upmanyu <siddhantkumarupmanyu@gmail.com>
2021-11-29 00:56:28 +0530

    spike test passing

--- app/src/androidTest/java/sku/challenge/itunesmusicsearch/ui/GalleryFragmentTest.kt
+++ app/src/androidTest/java/sku/challenge/itunesmusicsearch/ui/GalleryFragmentTest.kt
@@ -1,11 +1,17 @@
 package sku.challenge.itunesmusicsearch.ui
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import sku.challenge.itunesmusicsearch.R
+import sku.challenge.itunesmusicsearch.test_utils.DataBindingIdlingResourceRule
 import sku.challenge.itunesmusicsearch.test_utils.launchFragmentInHiltContainer
 
 
@@ -16,14 +22,25 @@ class GalleryFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @JvmField
+    @Rule
+    val dataBindingIdlingResourceRule = DataBindingIdlingResourceRule()
+
     @Before
-    fun setUp(){
+    fun setUp() {
         // Populate @Inject fields in test class
         hiltRule.inject()
 
         launchFragmentInHiltContainer<GalleryFragment> {
             dataBindingIdlingResourceRule.monitorFragment(this)
         }
+    }
+
+    @Test
+    fun testSpike() {
+        onView(withId(R.id.search_view)).perform(click())
+
+        Thread.sleep(2000)
     }
 
 }
