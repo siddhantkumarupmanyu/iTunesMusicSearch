671b66c85901fcfe843e9aa801f97d00acc4f5d2
Siddhant Kumar Upmanyu <siddhantkumarupmanyu@gmail.com>
2021-11-30 19:55:00 +0530

    Add GalleryViewModelTest.kt

---
+++ app/src/main/java/sku/challenge/itunesmusicsearch/ui/GalleryViewModel.kt
@@ -0,0 +1,32 @@
+package sku.challenge.itunesmusicsearch.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import sku.challenge.itunesmusicsearch.repository.SearchRepository
+import sku.challenge.itunesmusicsearch.vo.Track
+
+class GalleryViewModel(
+    private val searchRepository: SearchRepository
+) : ViewModel() {
+
+    private val _tracks = MutableStateFlow<TracksResult>(TracksResult.Success(emptyList()))
+
+    val tracks: StateFlow<TracksResult> = _tracks
+
+    fun search(query: String) {
+        viewModelScope.launch {
+            _tracks.value = TracksResult.Loading
+            val results = searchRepository.query(query)
+            _tracks.emit(TracksResult.Success(results))
+        }
+    }
+
+    sealed class TracksResult {
+        class Success(val tracks: List<Track>) : TracksResult()
+        object Loading : TracksResult()
+    }
+
+}
---
+++ app/src/test/java/sku/challenge/itunesmusicsearch/ui/GalleryViewModelTest.kt
@@ -0,0 +1,106 @@
+package sku.challenge.itunesmusicsearch.ui
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.yield
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.hamcrest.core.IsEqual.equalTo
+import org.hamcrest.core.IsInstanceOf
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import sku.challenge.itunesmusicsearch.repository.SearchRepository
+import sku.challenge.itunesmusicsearch.vo.Track
+
+@ExperimentalCoroutinesApi
+class GalleryViewModelTest {
+
+
+    private val tracks = listOf(
+        Track(1, "overdrive", "krsna", "https://example.com/thumbnail_over_krsna.jpg"),
+        Track(4, "overdrive", "prozpekt", "https://example.com/thumbnail_over_proz.jpg")
+    )
+    private val repository = FakeRepository()
+    private val viewModel = GalleryViewModel(repository)
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(StandardTestDispatcher())
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun searchTracks() = runTest {
+        assertThat(
+            viewModel.tracks.first(),
+            IsInstanceOf(GalleryViewModel.TracksResult.Success::class.java)
+        )
+
+        assertThat(
+            (viewModel.tracks.first() as GalleryViewModel.TracksResult.Success).tracks,
+            `is`(emptyList())
+        )
+
+        repository.query = "overdrive"
+        repository.delayBeforeReturningResult = 10L
+        repository.tracks = tracks
+
+        viewModel.search("overdrive")
+
+        // yield() or delay so that coroutine launched by viewModelScope is Run before asserting
+        // delay works since runTest uses virtual clock.
+        // so it suspends the current coroutine and runs the one with less
+        // more info see https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-test/kotlinx.coroutines.test/run-test.html
+        // and https://github.com/Kotlin/kotlinx.coroutines/blob/master/kotlinx-coroutines-test/README.md
+
+        yield()
+        assertThat(
+            viewModel.tracks.first(),
+            IsInstanceOf(GalleryViewModel.TracksResult.Loading::class.java)
+        )
+
+        // more delay relative to delayBeforeReturningResult, so that's run first
+        delay(20L)
+
+        assertThat(
+            viewModel.tracks.first(),
+            IsInstanceOf(GalleryViewModel.TracksResult.Success::class.java)
+        )
+
+        assertThat(
+            (viewModel.tracks.first() as GalleryViewModel.TracksResult.Success).tracks,
+            `is`(tracks)
+        )
+    }
+
+
+    // just because mockito does not allow suspend function while stubbing
+    // can use mockito-koltin but, just another dependency for only this simple case
+    class FakeRepository : SearchRepository {
+
+        var query: String = ""
+        var delayBeforeReturningResult: Long = 0
+        var tracks: List<Track> = emptyList()
+
+        override suspend fun query(query: String): List<Track> {
+            assertThat(query, `is`(equalTo(this.query)))
+
+            delay(delayBeforeReturningResult)
+
+            return tracks
+        }
+
+    }
+
+}
